5e8d1760402f2ca1d9b25e678cbf8ea2977fbde0
miguel-cortinas <118024590+miguel-cortinas@users.noreply.github.com>
2023-11-25 14:34:31 -0600

    add routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/index');
+
+/* GET users listing. */
+router.get('/', controller.home);
+
+router.post('/login', controller.login);
+
+
+module.exports = router;
